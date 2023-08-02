c0ea86249bae416d838a5ae2cbd2c98f33f8c709
Iamneo <techadmin@iamneo.ai>
2023-08-02 08:22:29 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/TaskController.java
+++ springapp/src/main/java/com/examly/springapp/TaskController.java
@@ -6,13 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin(origins="*")
-@RequestMapping("api/v1/")
+// @RequestMapping("api/v1/")
 public class TaskController {
     
     @Autowired
